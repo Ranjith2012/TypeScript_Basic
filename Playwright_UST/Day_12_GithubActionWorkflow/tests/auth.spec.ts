2f421d1f50f284c8f03f0e50766b8c9eab328c12
Ranjith2012 <ranjithkumar201200@gmail.com>
2025-05-02 12:07:48 +0530

    Github Actions practice

---
+++ Playwright_UST/Day_12_GithubActionWorkflow/tests/auth.spec.ts
@@ -0,0 +1,24 @@
+import { test as setup, expect } from '@playwright/test';
+import path from 'path';
+import fs from 'fs';
+import { Certificate } from 'crypto';
+
+const authFile = path.join(__dirname, '../tests/data.json');
+const credentials = JSON.parse(fs.readFileSync(authFile, 'utf-8'));
+
+setup('authenticate', async ({ page }) => {
+  // Perform authentication steps. Replace these actions with your own.
+  await page.goto('https://github.com/login');
+  await page.getByLabel('Username or email address').fill(credentials.username);
+  await page.getByLabel('Password').fill(credentials.password);
+  await page.locator('[value="Sign in"]').click();
+
+  // Wait until the page receives the cookies.
+  //
+  // Sometimes login flow sets cookies in the process of several redirects.
+  // Wait for the final URL to ensure that the cookies are actually set
+  await page.waitForURL('https://github.com');
+
+  // End of authentication steps.
+  await page.context().storageState({ path: authFile });
+});
