924c72138119d000093539a880c291948bb9bf5d
Ranjith2012 <ranjithkumar201200@gmail.com>
2025-05-02 12:52:00 +0530

    Github Actions working

--- Playwright_UST/Day_12_GithubActionWorkflow/tests/example.spec.ts
+++ Playwright_UST/Day_12_GithubActionWorkflow/tests/example.spec.ts
@@ -2,7 +2,6 @@ import { test, expect } from '@playwright/test';
 test('has title', async ({ page }) => {
   await page.goto('https://github.com/');
   await expect(await page.locator('#dashboard')).toBeVisible();
-  console.log('hello');
 });
 
 
