fc940c4ec189269fd29e8bc6efb9fdc25708b630
Ranjith2012 <ranjithkumar201200@gmail.com>
2025-05-05 12:51:36 +0530

    Mini project phase 2 updated

--- Playwright_UST/Day_13/tests/example.spec.ts
+++ Playwright_UST/Day_13/tests/example.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('User can Login With Valid Credentials', async ({ page }) => {
+  await page.goto('http://127.0.0.1:5500/src/loginpage.html');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
