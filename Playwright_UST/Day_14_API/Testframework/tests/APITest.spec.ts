c0bac2b7f3c2361ba759d1e987a3b9d625ef5f60
Ranjith2012 <ranjithkumar201200@gmail.com>
2025-05-07 17:30:01 +0530

    API Testing task completed 07/05

---
+++ Playwright_UST/Day_14_API/Testframework/tests/APITest.spec.ts
@@ -0,0 +1,57 @@
+import {test, expect , request } from '@playwright/test';
+let id:any;
+test('/ test', async ({ request }) => {
+    const res = await request.get('https://api.restful-api.dev/objects');
+    expect(res.ok()).toBeTruthy();
+    const resbody = await res.json();
+});
+
+test('Create the object',async ({request}) =>{
+    const res = await request.post('https://api.restful-api.dev/objects',{
+        data:{
+                "name": "Apple MacBook Pro 16",
+                "data": {
+                   "year": 2019,
+                   "price": 1849.99,
+                   "CPU model": "Intel Core i9",
+                   "Hard disk size": "1 TB"
+                },
+            }      
+    });
+    expect(res.ok()).toBeTruthy();
+    const resbody = await res.json();
+    id  = resbody.id;
+})
+
+test('Get the object',async({request})=>{
+    const res = await request.get(`https://api.restful-api.dev/objects/${id}`);
+    expect(res.ok()).toBeTruthy();
+    const resbody = await res.json();
+    console.log(resbody);
+})
+
+
+test('Update the object', async({request})=>{
+    const res = await request.put(`https://api.restful-api.dev/objects/${id}`,{
+        data:{
+            "name": "Dell Laptop",
+            "data": {
+                "year": 2019,
+                "price": 1849.99,
+                "CPU model": "Intel Core i11",
+                "Hard disk size": "1 TB"
+            },
+        }
+    })
+    expect(res.ok()).toBeTruthy();
+    const resbody = await res.json();
+    expect(resbody.name).toBe('Dell Laptop');
+    console.log(resbody);
+})
+
+test('Delete the object', async({request})=>{
+    const res = await request.delete(`https://api.restful-api.dev/objects/${id}`);
+    expect(res.ok()).toBeTruthy();
+    const resget = await request.get(`https://api.restful-api.dev/objects/${id}`);
+    expect(resget.ok()).toBeFalsy();
+})
---
+++ Playwright_UST/Day_14_API/Testframework/tests/example.spec.ts
@@ -0,0 +1,31 @@
+import { test, expect } from '@playwright/test';
+
+test('/ test', async ({ request }) => {
+  const res = await request.get('/');
+  expect(res.ok()).toBeTruthy();
+  const body = await res.body();
+  expect(body.toString()).toBe('Hello world');
+});
+
+test('/:name test', async ({ request }) => {
+  [
+    'Aaryan', 'Arush', 'Kirti',
+  ].forEach(async (name) => {
+    const res = await request.get(`/${name}`);
+    expect(res.ok()).toBeTruthy();
+    const body = await res.body();
+    expect(body.toString()).toBe(`Hello ${name}`);
+  })
+});
+
+test('/login test', async ({ request }) => {
+  const res = await request.post('/login', {
+    data: {
+      username: 'aryan',
+      pass: 123
+    }
+  });
+  expect(res.ok()).toBeTruthy();
+  const body = await res.body();
+  expect(body.toString()).toBe('Login success');
+});
